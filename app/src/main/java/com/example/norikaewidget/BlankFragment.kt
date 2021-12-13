df88fb259c625c6a3e4cd97a6ee9372936360eb3
pen <fushimi.chiba@gmail.com>
2021-12-13 19:46:50 +0900

    デバイスごとのサイズに応じて変形するよう変更。WidgetについてはLayoutのフレームの影響で修正できていない。

--- app/src/main/java/com/example/norikaewidget/BlankFragment.kt
+++ app/src/main/java/com/example/norikaewidget/BlankFragment.kt
@@ -12,6 +12,6 @@ class BlankFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        return inflater.inflate(R.layout.blanklayout, container, false)
     }
 }
