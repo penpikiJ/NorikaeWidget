ea267e3695c0874bfe227a6bd7a246c786c1309b
pen <fushimi.chiba@gmail.com>
2022-03-16 22:41:40 +0900

    ControlScheduleWidgetService.kt:24時をすぎた際に時刻表を正常取得できない問題をWidgetにも対応。

--- app/src/main/java/com/aaa/norikaewidget/ControlScheduleWidgetService.kt
+++ app/src/main/java/com/aaa/norikaewidget/ControlScheduleWidgetService.kt
@@ -19,6 +19,7 @@ import android.widget.RemoteViews
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 class ControlScheduleWidgetService : IntentService("ControlScheduleWidgetService") {
@@ -77,6 +78,9 @@ class ControlScheduleWidgetService : IntentService("ControlScheduleWidgetService
         var stationName = prefs.getString("Widget_Station",null)?.replace("駅","") +"駅"
         var routeName = prefs.getString("Widget_Route",null)
         val updown = prefs.getString("Widget_UpDown",null)
+
+        var stationTimeList_Weekday = prefs.getStringSet("SelectedTimeSchedule_Weekday", setOf())
+        var stationTimeList_Holiday = prefs.getStringSet("SelectedTimeSchedule_Holiday", setOf())
         // Construct the RemoteViews object
 
         //駅名・路線が５文字以上の場合は…で代替できるように変更
@@ -90,22 +94,63 @@ class ControlScheduleWidgetService : IntentService("ControlScheduleWidgetService
         views.setTextViewText(R.id.appwidget_text_route, routeName)
         views.setTextViewText(R.id.appwidget_text_updown,updown)
 
+//曜日の判定
+        val todayDaytype = LocalDate.now().dayOfWeek.value
+        val tomorrowDaytype = LocalDate.now().plusDays(1).dayOfWeek.value
 
-        val scheduleSet = prefs.getStringSet("SelectedTimeSchedule",null)
-        var a = 0
-        val scheduleList = scheduleSet?.toMutableList()
-        val sl = scheduleList?.get(a)?.split(",")
+        //曜日によって格納する時刻表を判断して格納するための変数
+        var todayStationTimeStringSet :Set<String> = setOf()
+        var tomorrowStationTimeStringSet :Set<String> = setOf()
+
+        if(todayDaytype == 6 or 7){
+            //"土・休日"
+            todayStationTimeStringSet = stationTimeList_Holiday!!
+        }else{
+            //"平日"
+            todayStationTimeStringSet = stationTimeList_Weekday!!
+        }
+
+        if(tomorrowDaytype == 6 or 7){
+            //"土・休日"
+            tomorrowStationTimeStringSet = stationTimeList_Holiday!!
+        }else{
+            //"平日"
+            tomorrowStationTimeStringSet = stationTimeList_Weekday!!
+        }
+
+        var todayScheduleSet = todayStationTimeStringSet
+
+        //本日・明日分を合わせた時刻表の作成先
         val launchTimeList:MutableList<LocalDateTime> = arrayListOf()
+        //以下で時刻表を作成
+        var a = 0
+        val todayScheduleList = todayScheduleSet?.toMutableList()
+        val slToday = todayScheduleList?.get(a)?.split(",")
+
         var i = 0
-        if (scheduleSet != null) {
-            while(i < sl!!.size){
-                val sList = LocalDateTime.parse(sl?.get(i)?.trim().
+        if (todayScheduleSet != null) {
+            while(i < slToday!!.size){
+                val sList = LocalDateTime.parse(slToday?.get(i)?.trim().
                 replace("[","")?.replace("]","")+":00")
                 sList.format(dtf)
                 launchTimeList.add(i,sList)
                 i++
             }
         }
+        var tomorrowScheduleSet = tomorrowStationTimeStringSet
+        val tomorrowScheduleList = tomorrowScheduleSet?.toMutableList()
+        val slTomorrow = todayScheduleList?.get(a)?.split(",")
+        if (tomorrowScheduleSet != null) {
+            while(i < slTomorrow!!.size){
+                val sList = LocalDateTime.parse(slTomorrow?.get(i)?.trim().
+                replace("[","")?.replace("]","")+":00")
+                sList.format(dtf)
+                launchTimeList.add(i,sList)
+                i++
+            }
+        }
+
+
         while(c< count) {
             c++
             if( destroyFlag == 1){
@@ -119,8 +164,9 @@ class ControlScheduleWidgetService : IntentService("ControlScheduleWidgetService
             while (x < launchTimeList.size) {
                 arrivalLocalDateTime =
                     now.with(LocalTime.of(launchTimeList[x].hour, launchTimeList[x].minute))
-                var min = ChronoUnit.MINUTES.between(now, arrivalLocalDateTime)
-                var sec = ChronoUnit.SECONDS.between(now, arrivalLocalDateTime) - min * 60
+                var min = ChronoUnit.MINUTES.between(now, arrivalLocalDateTime) % (24 * 60)
+                val sec = ChronoUnit.SECONDS.between(now,
+                    arrivalLocalDateTime) % (24 * 60 * 60) - min * 60
                 if (min >= 0) {
                     if (sec >= 0) {
                         //Widgetに時刻を設定
@@ -135,8 +181,8 @@ class ControlScheduleWidgetService : IntentService("ControlScheduleWidgetService
                             //次の１本までの時間表示
                         if( x+1 < launchTimeList.size){
                             val secondArrivalTime = now.with(LocalTime.of(launchTimeList[x+1].hour, launchTimeList[x+1].minute))
-                            var mintosecond = ChronoUnit.MINUTES.between(now, secondArrivalTime)
-                            var sectosecond = ChronoUnit.SECONDS.between(now, secondArrivalTime) - mintosecond * 60
+                            var mintosecond = ChronoUnit.MINUTES.between(now, secondArrivalTime) % (24 * 60)
+                            var sectosecond = ChronoUnit.SECONDS.between(now, secondArrivalTime) % (24 * 60 * 60) - mintosecond * 60
                             views.setTextViewText(
                                 R.id.rest_to_second,
                                 mintosecond.toString() + " " + "%02d".format(sectosecond)
@@ -145,8 +191,8 @@ class ControlScheduleWidgetService : IntentService("ControlScheduleWidgetService
                             //次の次までの時間表示
                         if( x+2 < launchTimeList.size){
                             val thirdArrivalTime = now.with(LocalTime.of(launchTimeList[x+2].hour, launchTimeList[x+2].minute))
-                            var mintothird = ChronoUnit.MINUTES.between(now, thirdArrivalTime)
-                            var sectothird = ChronoUnit.SECONDS.between(now, thirdArrivalTime) - mintothird * 60
+                            var mintothird = ChronoUnit.MINUTES.between(now, thirdArrivalTime) % (24 * 60)
+                            var sectothird = ChronoUnit.SECONDS.between(now, thirdArrivalTime) % (24 * 60 * 60) - mintothird * 60
                             views.setTextViewText(
                                 R.id.rest_to_third,
                                 mintothird.toString() + " " + "%02d".format(sectothird)
